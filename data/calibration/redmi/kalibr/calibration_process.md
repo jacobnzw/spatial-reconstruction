996c6bcc28206b2b671fc5a3aad58e488594ab09
Jakub Prüher <jacobnzw@users.noreply.github.com>
2026-07-27 15:34:54 +0200

    Feature/kalibr cam imu calibration (#23)
    * Add IMU pre-integration notes.
    * Move calibration chessboard images for redmi.
    * Add raw MARS Logger cam-imu recordings of apriltags.
    * Add CAM/IMU calibration results and reports from Kalibr.
    * Add notes recording commands and steps performed to calibrate the CAM/IMU on Redmi phone.
    * Fix calibration process note.

---
+++ data/calibration/redmi/kalibr/calibration_process.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c64abb865094b375576946a4d3c461813d0daccd295753f601cc708feacf040e
+size 1951
---
+++ data/calibration/redmi/kalibr/kalibr_from_mars.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3a98b72f829452a9195e2f07d521779a6b56aa226a9867880cc52a54b3ebc489
+size 3997
